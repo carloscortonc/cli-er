6464e1e4a2e1d2d0a254a3120658d3423c82e3ea
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2023-03-26 16:45:24 +0200

    fix: differentiate windows vs unix paths (#22)

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,5 @@
 import { findPackageJson } from "../src/utils";
+import path from "path";
 import fs from "fs";
 
 jest.mock("fs", () => ({
@@ -6,34 +7,50 @@ jest.mock("fs", () => ({
   existsSync: jest.fn(() => false),
 }));
 jest.mock("path", () => ({
+  esModule: true,
   ...jest.requireActual("path"),
-  join: (...parts: string[]) => parts.join("/"),
+  resolve: (...parts: string[]) => parts.join(path.sep),
   sep: "/",
 }));
+
+const mockPath = path as { sep: string };
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
 
 describe("findPackageJson", () => {
-  const options = { baseLocation: "base/location/path" };
-  it("Checks all directories from baseLocation upwards", () => {
+  const options = { baseLocation: "/base/location/path" };
+  it("[windows] Checks all directories from baseLocation upwards", () => {
+    mockPath.sep = "\\";
+    const pkgjson = findPackageJson({ baseLocation: "C:\\base\\location\\path" } as any);
+    expect((fs.existsSync as any).mock.calls).toEqual([
+      ["C:\\base\\location\\path\\package.json"],
+      ["C:\\base\\location\\package.json"],
+      ["C:\\base\\package.json"],
+      ["C:\\package.json"],
+    ]);
+    // Returns undefined if no package.json found
+    expect(pkgjson).toBe(undefined);
+  });
+  it("[unix] Checks all directories from baseLocation upwards", () => {
+    mockPath.sep = "/";
     const pkgjson = findPackageJson(options as any);
     expect((fs.existsSync as any).mock.calls).toEqual([
-      ["base/location/path/package.json"],
-      ["base/location/package.json"],
-      ["base/package.json"],
+      ["/base/location/path/package.json"],
+      ["/base/location/package.json"],
+      ["/base/package.json"],
     ]);
     // Returns undefined if no package.json found
     expect(pkgjson).toBe(undefined);
   });
   it("Returns the first package.json found", () => {
     const mockReadFile = (path: string) =>
-      ({
-        "base/location/path/package.json": undefined,
-        "base/location/package.json": '{"name": "location-package"}',
-        "base/package.json": '{"name": "base-package"}',
-      }[path]);
+    ({
+      "/base/location/path/package.json": undefined,
+      "/base/location/package.json": '{"name": "location-package"}',
+      "/base/package.json": '{"name": "base-package"}',
+    }[path]);
 
     (fs.existsSync as jest.Mock).mockImplementation(mockReadFile);
     (fs.readFileSync as jest.Mock).mockImplementation(mockReadFile);
