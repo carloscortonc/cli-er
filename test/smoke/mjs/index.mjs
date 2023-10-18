c39b0faf9ea7fffb188c53a592fd71a5de0fcc7d
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2023-10-18 20:15:46 +0200

    chore: remove assert syntax

--- test/smoke/mjs/index.mjs
+++ test/smoke/mjs/index.mjs
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import Cli from "../../../dist/index.js";
-import definition from "../definition.json" assert { type: "json" };
-import options from "../options.json" assert { type: "json" };
+import definition from "../definition.json";
+import options from "../options.json";
 
 new Cli(definition, options).run();
