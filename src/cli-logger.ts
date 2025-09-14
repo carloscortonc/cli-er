565b8ec5c5810ebb49abd79b06100e249f645492
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2025-09-14 11:50:19 +0200

    feat: update default logger & switch to static

--- src/cli-logger.ts
+++ src/cli-logger.ts
@@ -1,11 +1,9 @@
-import { ICliLogger } from "./types";
-
 /** Class implementing the ICliLogger interface */
-export default class CliLogger implements ICliLogger {
-  log = (...message: any[]) => {
-    process.stdout.write("".concat(message.join(" ")));
+export default class CliLogger {
+  static log = (...message: any[]) => {
+    process.stdout.write("".concat(message.join("")));
   };
-  error = (...message: any[]) => {
-    process.stderr.write("ERROR ".concat(message.join(" ")));
+  static error = (...message: any[]) => {
+    process.stderr.write("ERROR ".concat(message.join("")));
   };
 }
