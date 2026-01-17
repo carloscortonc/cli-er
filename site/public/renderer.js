69aa55820da14d52a01641b9995db8cb149ae5c1
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2026-01-17 16:44:40 +0100

    feat: keep output

---
+++ site/public/renderer.js
@@ -0,0 +1,19 @@
+let [i, o] = ["input", "output"].map((id) => document.getElementById(id));
+
+export function renderInput(value) {
+  const input = document.createElement("div");
+  input.className = "input-wrapper";
+  input.innerHTML = `$<span>${value}</span>`;
+  o.appendChild(input);
+}
+
+export function renderOutput(value, error) {
+  const e = document.createElement("pre");
+  e.innerText = value;
+  error && (e.className = "error");
+  o.appendChild(e);
+}
+
+export function clearOutput() {
+  o.innerHTML = "";
+}
