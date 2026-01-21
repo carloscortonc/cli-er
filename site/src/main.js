e8812f1bc345417ffbc571193c4440d30fd8aecd
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2026-01-21 20:41:42 +0100

    chore: organize content

---
+++ site/src/main.js
@@ -0,0 +1,63 @@
+import handleKey from "./key-handler.js";
+import * as renderer from "./renderer.js";
+import * as history from "./history.js";
+import "./index.css";
+
+const builtins = { history: history.spec, clear: renderer.clearSpec };
+for (const c of Object.keys(builtins)) {
+  window.CLI_COMMANDS[c] = builtins[c];
+}
+
+// Create handler for command actions
+const blobSource = "export default (...args) => window.CLI_ACTION_REF(...args);";
+const blob = new Blob([blobSource], { type: "text/javascript" });
+const url = URL.createObjectURL(blob);
+require("url").pathToFileURL = () => ({ href: url });
+
+let [i, o, oa] = ["input", "output", "output-after"].map((id) => document.getElementById(id));
+document.addEventListener("click", () => i.focus());
+o.addEventListener("click", (e) => e.stopPropagation());
+
+// Capture cli output
+process.stdout.write = (v) => renderer.renderOutput(v);
+process.stderr.write = (v) => renderer.renderOutput(v, { error: true });
+
+handleKey(i, {
+  Enter: () => {
+    const [cmd, ...args] = i.value.split(" ");
+    if (!cmd) return;
+    renderer.renderInput(i.value);
+    history.add(i.value);
+    i.value = "";
+
+    const cliSpec = CLI_COMMANDS[cmd];
+    if (!cliSpec) {
+      return process.stderr.write(`Command not found: "${cmd}"`);
+    }
+
+    window.CLI_ACTION_REF = cliSpec[2];
+    new Cli(cliSpec[0], { ...cliSpec[1], cliName: cmd }).run(args);
+  },
+  Tab: (e) => {
+    e.preventDefault();
+    const candidates = Object.keys(CLI_COMMANDS).filter((k) => k.startsWith(i.value));
+    if (!candidates.length) return;
+    if (candidates.length === 1) {
+      i.value = candidates[0];
+      return renderer.clearOutput(oa);
+    }
+    renderer.updateOutput(candidates.join("  "));
+  },
+  ArrowUp: (e) => {
+    e.preventDefault();
+    let p = history.previous();
+    if (!p) return;
+    renderer.updateInputValue(p);
+  },
+  ArrowDown: (e) => {
+    e.preventDefault();
+    let n = history.next();
+    if (!n) return;
+    renderer.updateInputValue(n);
+  },
+});
